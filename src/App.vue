246bc21d8c3f52627d475ef308da2db072b31772
rorik <rorik302@gmail.com>
2022-01-09 15:02:46 +0300

    установлены зависимости, сделаны настройки проекта

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-
+  <el-config-provider :locale="locale"></el-config-provider>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue"
+  import { defineComponent } from "vue"
+  import locale from "element-plus/es/locale/lang/ru"
 
-export default defineComponent({
-  setup() {
-
-  }
-})
+  export default defineComponent({
+    setup() {
+      return {
+        locale
+      }
+    }
+  })
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
